ed6ca19954e58f86b5c69cdbdac58b40c71202d8
Sam Wyness <sam.wyness@district-technologies.com>
2021-09-14 08:53:29 +1000

    feat(utils): add log message constants

---
+++ src/utils/logMessages.ts
@@ -0,0 +1,27 @@
+import chalk from 'chalk'
+
+const needHelpWithTokensConfig = `if you need help creating a figma-tokens.yml, try running ${chalk.bold(
+  `'sw-tokens init'`
+)} in your terminal`
+
+const tokensConfigGeneric =
+  `Something went wrong when loading your your './figma-tokens.yml' file, ` +
+  needHelpWithTokensConfig
+
+const invalidTokensConfig =
+  `Invalid yaml config detected, please check that your './figma-tokens.yml' is correct, ` +
+  needHelpWithTokensConfig
+
+const noDocumentFoundInFigmaFile = (fileId: string) =>
+  `No DOCUMENT found in Figma file ${chalk.blue(fileId)}`
+
+const unsupportedNodeType = (nodeType: FigmaNode['type']) =>
+  `The Figma node type '${nodeType}' is not supported. We currently support 'COMPONENT_SET' node types`
+
+export const logMessages = {
+  needHelpWithTokensConfig,
+  tokensConfigGeneric,
+  invalidTokensConfig,
+  noDocumentFoundInFigmaFile,
+  unsupportedNodeType,
+}
